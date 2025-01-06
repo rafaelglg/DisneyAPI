0d2edaf6ce257fbe43446932c871b569109d2c32
rafael <rloggiodice@eservicios.indra.es>
2025-01-06 21:03:13 +0100

    Initial Commit

---
+++ DisneyAPI/DisneyAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyAPIApp.swift
+//  DisneyAPI
+//
+//  Created by Rafael Loggiodice on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DisneyAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
