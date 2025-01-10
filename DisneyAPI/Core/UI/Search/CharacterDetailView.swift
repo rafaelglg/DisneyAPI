b87b788f1ebd090c9c628123190cb842ebc2470a
rafael <rloggiodice@eservicios.indra.es>
2025-01-10 23:24:23 +0100

    [feature] create characterDetailView

--- DisneyAPI/Core/UI/Search/CharacterDetailView.swift
+++ DisneyAPI/Core/UI/Search/CharacterDetailView.swift
@@ -12,26 +12,53 @@ struct CharacterDetailView: View {
     let character: CharacterDataResponse
     
     var body: some View {
-        VStack {
-            ImageLoaderView(urlString: character.imageUrl ?? "")
-                .frame(width: 200, height: 300)
-            
-            Text(character.name ?? "")
-                .font(.title2)
-                .bold()
- 
+        ScrollView {
+            VStack {
+                
+                ImageLoaderView(urlString: character.imageUrl ?? "")
+                    .frame(width: UIScreen.main.bounds.height / 2, height: 600)
+                
+                characterName
+                
+                if let sourceUrl = character.sourceUrl, !sourceUrl.isEmpty {
+                    Text("More info")
+                        .font(.title2)
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 40)
+                        .padding(.top)
+                    Link(destination: URL(string: sourceUrl)!) {
+                        Text(sourceUrl)
+                    }
+                }
+                
+            }
         }
+        .ignoresSafeArea()
+    }
+    
+    private var characterName: some View {
+        Text(character.name ?? "")
+            .font(.title)
+            .bold()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 40)
+            .padding(.top)
     }
 }
 
 #Preview("Production") {
     @Previewable @State var viewModel = SearchViewModelImpl(interactor: CoreInteractor(characterRepository: CharacterServiceImpl()))
     
-    VStack {
-        CharacterDetailView(character: viewModel.allCharacters.first ?? .mock)
-            .task {
-                await viewModel.getAllCharacters()
-            }
+    ZStack {
+        if viewModel.isLoading {
+            ProgressView()
+        } else {
+            CharacterDetailView(character: viewModel.allCharacters.first ?? .mock)
+        }
+    }
+    .task {
+        await viewModel.getAllCharacters()
     }
 }
 
